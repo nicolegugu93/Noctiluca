807ebea4538feefac7fd19b877d1bb9a4535ff6d
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-07-21 08:36:28 -0500

    router(sin ningun cambio, solo porque me dice que tengo que hacer un commit)

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,6 +7,7 @@ import Home from "../pages/Home";
 import ContactButterfly from "../pages/ContactButterfly";
 
 
+
 const routerButterfly =createBrowserRouter ([{
     path:"/",
     element: <Layout/>,
