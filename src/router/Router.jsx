94394b2e2c37667ccb1d722d7bfab3fcb2842684
nicolegugu93 <nicole.guevaragutierrez@gmail.com>
2025-07-21 20:22:42 +0200

    solving problems at route and it pages

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import EditButterfly from "../pages/EditButterfly";
 import CreateButterfly from "../pages/CreateButterfly";
-import ButterflyDetail from "../pages/ButterflyDetail";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
 import ContactButterfly from "../pages/ContactButterfly";
