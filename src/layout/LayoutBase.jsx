7afec7b0fd62e6821e975f71eff754e1292cc3ab
MaryoriCruz <maryori.eguizabal@gmail.com>
2025-07-29 13:44:21 -0500

    feat(Layout): add footer in the LayoutBase page(#17)

--- src/layout/LayoutBase.jsx
+++ src/layout/LayoutBase.jsx
@@ -1,9 +1,15 @@
 // src/layout/LayoutBase.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const LayoutBase = () => {
-    return <Outlet />;
+    return (
+    <>
+    <Outlet />
+    <Footer />
+    </>
+    );
 };
 
 export default LayoutBase;
