763d4947c0d113dbac21838804a2c6558a7f0661
MarianaMH1195 <morenomariana03@gmail.com>
2025-07-29 16:22:29 +0200

    fix responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import '../style/NavBar.css';
 import alaImg from "../assets/logo-mariposa.png";
 
 function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -12,30 +13,37 @@ function NavBar() {
 
   return (
     <nav className="nav-container">
-      {/* NAVBAR FIJA */}
-      <div className="galeria-extra-buttons">
-        <img src={alaImg} alt="ala mariposa" className="ala-mariposa" />
+      <div className="gallery-extra-buttons">
+        <img src={alaImg} alt="butterfly-wing" className="butterfly-wing" />
 
-        {/* Botón hamburguesa visible solo en móvil */}
         <button className="hamburger" onClick={toggleMenu}>
           ☰
         </button>
 
-        {/* Botones de navegación */}
-        <div className={`galeria-buttons-group ${menuOpen ? "open" : ""}`}>
-          <Link to="/" className="galeria-button">Home</Link>
-          <Link to="/contactbutterfly" className="galeria-button">Contacto</Link>
+        <div className={`gallery-buttons-group ${menuOpen ? "open" : ""}`}>
+          
+          <Link to="/" className="gallery-button">Home</Link>
 
+          
+          {location.pathname === "/contactbutterfly" && (
+            <Link to="/butterflygallery" className="gallery-button">Galería</Link>
+          )}
+
+          
+          {(location.pathname === "/butterflygallery" || location.pathname === "/") && (
+            <Link to="/contactbutterfly" className="gallery-button">Contacto</Link>
+          )}
         </div>
       </div>
-      {/* Línea decorativa debajo del navbar */}
+
+     
       <div className="navbar-line"></div>
     </nav>
   );
-
 }
 
-
-
 export default NavBar;
+
+
+
 
