2441e61a4cd467e338bdc09bd69041641ad05470
MarianaMH1195 <morenomariana03@gmail.com>
2025-07-29 12:25:01 +0200

    refactor layout router

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,25 @@ export default function Footer() {
   return (
     <>
       <div className="footer-line"></div> {/* LÍNEA DECORATIVA */}
-      
-      <footer className="bg-indigo-950 text-white text-center py-6 text-sm mt-auto">
-        <p>
-          © 2025 · Proyecto realizado por Nicole, Maryori, Valentina, Esther, Rocío y Mariana para el Bootcamp Fullstack
-          (Frontend + Backend) de Factoria F5. Todo los contenidos tienen fines educativos y de divulgación.
-        </p>
+
+      <footer className="bg-indigo-950 text-white py-6 text-sm mt-auto">
+        <div className="footer-content">
+          <p>
+            © 2025 · Proyecto realizado por Nicole, Maryori, Valentina, Esther, Rocío y Mariana para el Bootcamp Fullstack
+            (Frontend + Backend) de Factoria F5. Todo los contenidos tienen fines educativos y de divulgación.
+          </p>
+
+          <a href="https://factoriaf5.org/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="footer-logo"
+              src="https://femcoders.factoriaf5.org/wp-content/uploads/2021/12/Logo-FemCodersF5-color-300x224.png"
+              alt="logo femcoders factoriaf5"
+            />
+          </a>
+        </div>
       </footer>
     </>
   );
 }
+
 
