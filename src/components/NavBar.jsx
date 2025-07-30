36da1a338c327498f1dffc7f5b1ea8340bab9168
MarianaMH1195 <morenomariana03@gmail.com>
2025-07-30 15:47:12 +0200

    Removed custom styles from hamburger button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../style/NavBar.css';
 import alaImg from "../assets/logo-mariposa.png";
 
 function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const location = useLocation();
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -21,30 +20,12 @@ function NavBar() {
         </button>
 
         <div className={`galeria-buttons-group ${menuOpen ? "open" : ""}`}>
-          {/* Botón siempre visible */}
           <Link to="/" className="galeria-button">Home</Link>
-
-          {/* Mostrar "Galería" solo si estás en Contacto */}
-          {location.pathname === "/contactbutterfly" && (
-            <Link to="/butterflygallery" className="galeria-button">Galería</Link>
-          )}
-
-          
-           {/* Mostrar "Galería" en formulario de mariposas*/}
-          {location.pathname.startsWith("/butterflydetail")&& (
-            <Link to="/butterflygallery" className="galeria-button">Galería</Link>
-          )}
-           
-
-
-          {/* Mostrar "Contacto" solo si estás en Galería o Home */}
-          {(location.pathname === "/butterflygallery" || location.pathname === "/") && (
-            <Link to="/contactbutterfly" className="galeria-button">Contacto</Link>
-          )}
+          <Link to="/butterflygallery" className="galeria-button">Galería</Link>
+          <Link to="/contactbutterfly" className="galeria-button">Contacto</Link>
         </div>
       </div>
 
-      {/* Línea decorativa debajo del nav */}
       <div className="navbar-line"></div>
     </nav>
   );
